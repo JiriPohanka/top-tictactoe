6c2fbc1df46e6bceaa93e307b6d033bc306db03b
Jiri Pohanka <jiri.pohanka@gmail.com>
2021-08-03 22:43:07 +0200

    Add logic for player turns, gameboard module and updating dom with player marks

--- app.js
+++ app.js
@@ -1,29 +1,57 @@
+const gameBoxes = document.querySelectorAll(".play-box");
 
 // gameboard [] populates grid in html using data-box-id
 const gameBoard = (() => {
-    const playerMoves = [["x"], ["x"], ["o"], ["o"], ["x"], ["x"], ["o"], ["x"], ["o"]];
-    const gameBoxes = document.querySelectorAll(".play-box");
+    const playerMoves = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
-    for ([i, gameBox] of gameBoxes.entries()) {
-        console.log(i + " :" + gameBox);
-        gameBox.innerHTML = playerMoves[i];
+    const roundsPlayed = 0;
+
+    const updatePlayerMoves = function (i, value) {
+        playerMoves.splice(i, 1, value);
+        roundsPlayed++;
     }
+ 
+    return {updatePlayerMoves};
 })();
 
+//attach listeners to divs in grid
+const attachEventListenersToPlayField = () => {
+    for ([i, gameBox] of gameBoxes.entries()) {
+
+        gameBox.addEventListener('click', (e) => {
+
+            const gameBoxId = e.target.dataset.boxId;
+
+            if (player1.playCount <= player2.playCount) {
+                player1.makeMove(gameBoxId);
+            } else {
+                player2.makeMove(gameBoxId);
+            }
+        })
+    }
+}
 
 //factory function to create players
 const newPlayer = (playerName, mark) => {
-    return {playerName, mark}
+    let playCount = 0;
+
+    function makeMove (gameBoxId) {
+        const gameBoxTextContent = document.querySelector(`[data-box-id='${gameBoxId}']`).textContent;
+        if ( gameBoxTextContent === "x" || gameBoxTextContent === "o" ) {
+        } else {
+            document.querySelector(`[data-box-id='${gameBoxId}']`).textContent = mark;
+            gameBoard.updatePlayerMoves(gameBoxId, mark);
+            this.playCount++;
+        }
+    }
+
+    return {playCount, makeMove}
 }
 
 const player1 = newPlayer("player1", "x");
 const player2 = newPlayer("player2", "o");
 
-
-//players need to take turns somehow
-
-//each time player clicks a div, his "X or O" gets added to the gameboard []
-
-
-
-
+//on load 
+window.addEventListener('load', () => {
+    attachEventListenersToPlayField();
+})
